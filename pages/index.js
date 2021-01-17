156e4fb1ad173321bf48f34a931190dff5c97d72
dheeraj <dheeraj@pennyworthwizz.com>
2021-01-17 17:14:43 +0530

    issue fixed

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,10 @@ import YearCardComponent from "../components/YearCardComponent";
 import MissionCardComponent from "../components/MissionCardComponent";
 import { Grid } from "@material-ui/core";
 import styles from "../components/style.module.css";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
-const Home = () => {
-  const [productData, setProductData] = useState([]);
+const Home = ({ data }) => {
+  const [productData, setProductData] = useState(data);
   const [selectedYear, setYear] = useState("");
   const [launchValue, setLaunchValue] = useState("");
   const [landingValue, setLandingValue] = useState("");
@@ -27,33 +27,25 @@ const Home = () => {
         setLandingValue("");
     }
   };
-  useEffect(() => {
-    fetch("https://api.spaceXdata.com/v3/launches?limit=100")
-      .then((response) => response.json())
-      .then((data) => setProductData(data))
-      .catch((err) => {
-        console.log("NETWORK ERROR", err);
-      });
-  }, []);
-  useCallback(() => {
-    let filteredUrl = `https://api.spaceXdata.com/v3/launches?limit=100&launch_success=${launchValue}&land_success=${landingValue}&launch_year=${selectedYear}`;
-    if (launchValue && selectedYear === "" && !landingValue) {
-      filteredUrl =
-        "https://api.spaceXdata.com/v3/launches?limit=100&launch_success=true";
-    } else if (launchValue && landingValue && selectedYear === "") {
-      filteredUrl =
-        "https://api.spaceXdata.com/v3/launches?limit=100&launch_success=true&land_success=true";
-    } else if (launchValue && selectedYear !== "" && landingValue) {
-      filteredUrl = `https://api.spaceXdata.com/v3/launches?limit=100&launch_success=true&land_success=true&&launch_year=${selectedYear}`;
-    }
+  // useEffect(() => {
+  //   let filteredUrl = `https://api.spaceXdata.com/v3/launches?limit=100&launch_success=${launchValue}&land_success=${landingValue}&launch_year=${selectedYear}`;
+  //   if (launchValue && selectedYear === "" && !landingValue) {
+  //     filteredUrl =
+  //       "https://api.spaceXdata.com/v3/launches?limit=100&launch_success=true";
+  //   } else if (launchValue && landingValue && selectedYear === "") {
+  //     filteredUrl =
+  //       "https://api.spaceXdata.com/v3/launches?limit=100&launch_success=true&land_success=true";
+  //   } else if (launchValue && selectedYear !== "" && landingValue) {
+  //     filteredUrl = `https://api.spaceXdata.com/v3/launches?limit=100&launch_success=true&land_success=true&&launch_year=${selectedYear}`;
+  //   }
 
-    fetch(filteredUrl)
-      .then((response) => response.json())
-      .then((data) => setProductData(data))
-      .catch((err) => {
-        console.log("NETWORK ERROR", err);
-      });
-  }, [selectedYear, launchValue, landingValue]);
+  //   fetch(filteredUrl)
+  //     .then((response) => response.json())
+  //     .then((data) => setProductData(data))
+  //     .catch((err) => {
+  //       console.log("NETWORK ERROR", err);
+  //     });
+  // }, [selectedYear, launchValue, landingValue]);
 
   return (
     <Grid container item className={styles.containerCard}>
@@ -68,16 +60,29 @@ const Home = () => {
         lg={10}
         className={styles.containerCard}
       >
-        {productData.map((val) => {
+        {/* {productData.map((val) => {
           return (
             <Grid item={true} key={val.flight_number} xs={12} sm={6} lg={3}>
               <MissionCardComponent {...val} />
             </Grid>
           );
-        })}
+        })} */}
       </Grid>
     </Grid>
   );
 };
 
+// export async function getServerSideProps() {
+//   try {
+//     const res = await fetch("https://api.spaceXdata.com/v3/launches?limit=100");
+//     const data = await res.json();
+//     return {
+//       props: {
+//         data,
+//       },
+//     };
+//   } catch (err) {
+//     console.log("SEVER ERROR");
+//   }
+// }
 export default Home;
