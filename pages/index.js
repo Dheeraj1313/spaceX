80b02b52a65fadb787e309111d5a2053cc01b232
dheeraj <dheeraj@pennyworthwizz.com>
2021-01-17 17:25:32 +0530

    issue fixed

--- pages/index.js
+++ pages/index.js
@@ -27,25 +27,25 @@ const Home = ({ data }) => {
         setLandingValue("");
     }
   };
-  // useEffect(() => {
-  //   let filteredUrl = `https://api.spaceXdata.com/v3/launches?limit=100&launch_success=${launchValue}&land_success=${landingValue}&launch_year=${selectedYear}`;
-  //   if (launchValue && selectedYear === "" && !landingValue) {
-  //     filteredUrl =
-  //       "https://api.spaceXdata.com/v3/launches?limit=100&launch_success=true";
-  //   } else if (launchValue && landingValue && selectedYear === "") {
-  //     filteredUrl =
-  //       "https://api.spaceXdata.com/v3/launches?limit=100&launch_success=true&land_success=true";
-  //   } else if (launchValue && selectedYear !== "" && landingValue) {
-  //     filteredUrl = `https://api.spaceXdata.com/v3/launches?limit=100&launch_success=true&land_success=true&&launch_year=${selectedYear}`;
-  //   }
+  useEffect(() => {
+    let filteredUrl = `https://api.spaceXdata.com/v3/launches?limit=100&launch_success=${launchValue}&land_success=${landingValue}&launch_year=${selectedYear}`;
+    if (launchValue && selectedYear === "" && !landingValue) {
+      filteredUrl =
+        "https://api.spaceXdata.com/v3/launches?limit=100&launch_success=true";
+    } else if (launchValue && landingValue && selectedYear === "") {
+      filteredUrl =
+        "https://api.spaceXdata.com/v3/launches?limit=100&launch_success=true&land_success=true";
+    } else if (launchValue && selectedYear !== "" && landingValue) {
+      filteredUrl = `https://api.spaceXdata.com/v3/launches?limit=100&launch_success=true&land_success=true&&launch_year=${selectedYear}`;
+    }
 
-  //   fetch(filteredUrl)
-  //     .then((response) => response.json())
-  //     .then((data) => setProductData(data))
-  //     .catch((err) => {
-  //       console.log("NETWORK ERROR", err);
-  //     });
-  // }, [selectedYear, launchValue, landingValue]);
+    fetch(filteredUrl)
+      .then((response) => response.json())
+      .then((data) => setProductData(data))
+      .catch((err) => {
+        console.log("NETWORK ERROR", err);
+      });
+  }, [selectedYear, launchValue, landingValue]);
 
   return (
     <Grid container item className={styles.containerCard}>
@@ -60,29 +60,30 @@ const Home = ({ data }) => {
         lg={10}
         className={styles.containerCard}
       >
-        {/* {productData.map((val) => {
+        {productData.map((val) => {
           return (
             <Grid item={true} key={val.flight_number} xs={12} sm={6} lg={3}>
               <MissionCardComponent {...val} />
             </Grid>
           );
-        })} */}
+        })}
       </Grid>
     </Grid>
   );
 };
 
-// export async function getServerSideProps() {
-//   try {
-//     const res = await fetch("https://api.spaceXdata.com/v3/launches?limit=100");
-//     const data = await res.json();
-//     return {
-//       props: {
-//         data,
-//       },
-//     };
-//   } catch (err) {
-//     console.log("SEVER ERROR");
-//   }
-// }
+export async function getServerSideProps() {
+  let url = "https://api.spaceXdata.com/v3/launches?limit=100";
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    return {
+      props: {
+        data,
+      },
+    };
+  } catch (err) {
+    console.log("SEVER ERROR");
+  }
+}
 export default Home;
